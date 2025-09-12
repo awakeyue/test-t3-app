a43f09948c255e31a578cddc60630121cbe4a681
v_yanpyue <v_yanpyue@tencent.com>
2025-09-12 09:45:52 +0800

    feat: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#185a13] to-[#15162c] text-white"></main>
+  );
+}
